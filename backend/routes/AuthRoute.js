a9d69d4089821579e4348ec74c720d53d1df872e
Rohan <rohanprajjapati@gmail.com>
2026-07-20 23:49:31 +0530

    otp with sms

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -6,8 +6,8 @@ const AuthController=require("./../controllers/AuthController")
 const adminMiddleware = require("./../middlewares/adminMiddleware")
 router.post("/register",upload.single("image"), AuthController.register)
 router.post("/login", AuthController.login)
-router.post("/send-otp", sendOtp);
-router.post("/verify-otp", verifyOtp);
+router.post("/send-otp", AuthController.sendOtp);
+router.post("/verify-otp",AuthController.verifyOtp );
 
 
 module.exports=router   
