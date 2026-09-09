0b8a27cd90189c84356664960939329fd1c2c5e4
Rohan <rohanprajjapati@gmail.com>
2026-09-09 18:21:25 +0530

    Fix path-to-regexp syntax error in server.js

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,6 @@ const io = new Server(server, {
 app.set("io", io);
 
 app.use(cors(corsOption));
-app.options("*", cors(corsOption));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
